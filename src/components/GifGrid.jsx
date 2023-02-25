0d9d542b905081cf98929aa1c73e4993f388e70a
unknown <Victor@dde-vaguilar.grupo-bf.local>
2023-02-25 10:22:59 -0300

    nuevo componente

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,36 +1,23 @@
-import React, { useEffect, useState } from 'react';
 import { GifItem } from "./GifItem";
-import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 
 export const GifGrid = ({ category }) => {
     
-    const [ images, setImages ] = useState([]);
-
-    const getImages = async() => {
-        const newImages = await getGifs ( category );
-        setImages(newImages);
-    }
-
-    console.log(images)
-
-
-
-    useEffect( ()=> {
-        //getGifs(category)
-        getImages();
-    }, [] );
+    const { images , isLoading } = useFetchGifs( category );
     
-    //getGifs(category);
-
+    //console.log( {images , isLoading })
     return (
         <>
             <h3>{category}</h3>
+            {isLoading &&( <h2>Cargando...</h2>)}
+            
+
+
             <div className="card-grid">{
                     images.map( (image) => (
                         <GifItem key={image.id}
                         { ... image}
-                        
                         />
                     ))
                 }
--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,11 +1,11 @@
 
 export const GifItem = ({id,title,url}) => {
   
-  console.log(url)
+  //console.log(url)
     return (
-    //<li key={ id }>{ title }</li>
     <div className="card">
         <img src={ url } alt={ title } />
+        <p>{title}</p>
     </div>
   )
 }
